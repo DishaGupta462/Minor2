836f1f73df1b53fe7b9400b5470712e02cede7c2
DishaGupta462 <78653877+DishaGupta462@users.noreply.github.com>
2023-04-24 22:21:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from doctest import OutputChecker
+from msilib.schema import File
+from PIL import Image
+import numpy as np
+
+import encrypt_decrypt_func as EDF
+import key_generator as KG
+
+
+publickey, privatekey = KG.generate_keys()
+print(publickey.__repr__())
+
+im = Image.open(r"C:\Users\Disha Gupta\Documents\Sem 6\Crypto Project\Homomorphic-Image-Encryption\test-images\Encrypt.jpg") 
+im.show()
+img = Image.open(r"C:\Users\Disha Gupta\Documents\Sem 6\Crypto Project\Homomorphic-Image-Encryption\test-images\Decrypt.jpg") 
+img.show()
+
+
+encrypt_image = EDF.image_encryption(publickey,im)
+EDF.show_encrypted_image(encrypt_image)
+
+
+
+inc_bright = EDF.increase_brightness(publickey,encrypt_image,30)
+im = EDF.image_decryption(publickey,privatekey,inc_bright)
+im.show()
